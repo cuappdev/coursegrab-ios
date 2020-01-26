108996f0eb8790da8d645c8b9c81680e2a6754ee
= <daniel.vebman@gmail.com>
2020-01-26 13:42:41 -0500

    Initial Commit

---
+++ CourseGrab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CourseGrab
+//
+//  Created by Daniel Vebman on 1/26/20.
+//  Copyright © 2020 Cornell AppDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
