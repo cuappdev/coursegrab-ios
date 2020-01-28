9eaed0bb77bf352964c7f48522f22485b9f295a3
Reade <readeplunkett@gmail.com>
2020-01-28 16:45:52 -0500

    Build LoginViewController and CoursesNavigationController (#1)
    * Builds out loginViewController and CoursesNavigationController
    
    * Address PR comments
    
    * Refactor Keys.swift
    
    * Address PR comments in Keys.swift

---
+++ CourseGrab/Keys.swift
@@ -0,0 +1,21 @@
+//
+//  Keys.swift
+//  CourseGrab
+//
+//  Created by Reade Plunkett on 1/28/20.
+//  Copyright © 2020 Cornell AppDev. All rights reserved.
+//
+
+import Foundation
+
+struct Keys {
+    
+    static let googleClientID = Keys.keyDict["google-client-id"] as! String
+
+    private static let keyDict: NSDictionary = {
+        guard let path = Bundle.main.path(forResource: "Keys", ofType: "plist"),
+            let dict = NSDictionary(contentsOfFile: path) else { return [:] }
+        return dict
+    }()
+
+}
---
+++ CourseGrab/MainNavigationController.swift
@@ -0,0 +1,24 @@
+//
+//  CoursesNavigationController.swift
+//  CourseGrab
+//
+//  Created by Reade Plunkett on 1/26/20.
+//  Copyright © 2020 Cornell AppDev. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationBar.barTintColor = .black
+        navigationBar.tintColor = .white
+        navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationBar.isTranslucent = false
+        navigationBar.barStyle = .black
+    }
+
+}
