b21ef57f5ce7f886802f848fd097a30015938d1d
= <daniel.vebman@gmail.com>
2020-02-05 19:54:23 -0500

    Start pull interaction

--- CourseGrab/ViewControllers/Settings/SettingsViewController.swift
+++ CourseGrab/ViewControllers/Settings/SettingsViewController.swift
@@ -24,17 +24,77 @@ class SettingsViewController: UIViewController {
     }
 
     override func viewDidLoad() {
-//        modalPresentationStyle = .fullScreen
-//        view.frame = CGRect(x: 0, y: view.frame.height / 2, width: view.frame.width, height: view.frame.height / 2)
-        view.backgroundColor = .clear
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
 
-        contentView.backgroundColor = .red
+        contentView.backgroundColor = .white
         view.addSubview(contentView)
         contentView.snp.makeConstraints { make in
             make.bottom.equalToSuperview()
             make.width.equalToSuperview()
-            make.height.equalToSuperview().dividedBy(2)
+            make.height.equalTo(300)
         }
+
+        view.pan(pan)
+    }
+
+    private func pan(_ gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .changed:
+            let translation = gesture.translation(in: view).y
+            contentView.transform = CGAffineTransform(translationX: 0, y: min(max(0, translation), contentView.frame.height))
+        case .ended, .cancelled:
+            let velocity = gesture.velocity(in: view).y
+            if velocity > 0 {
+                dismiss(velocity: velocity)
+            } else {
+                UIView.animate(withDuration: 0.3) {
+                    self.contentView.transform = .identity
+                }
+            }
+        default:
+            break
+        }
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        view.backgroundColor = .clear
+
+        contentView.roundCorners(corners: [.topLeft, .topRight], radius: 20)
+        contentView.transform = CGAffineTransform(translationX: 0, y: contentView.frame.height)
+
+        UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseOut, animations: {
+            self.contentView.transform = .identity
+        })
+        UIView.animate(withDuration: 0.1, animations: {
+            self.view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        })
+    }
+
+    private func dismiss(velocity: CGFloat = 1) {
+        let yPoints = contentView.frame.height - contentView.transform.ty
+        let duration = max(0.1, TimeInterval(yPoints / velocity))
+
+        print(yPoints)
+
+        UIView.animate(withDuration: duration) {
+//            self.view.backgroundColor = .clear
+        }
+        UIView.animate(withDuration: duration, animations: {
+            self.contentView.transform = self.contentView.transform.translatedBy(x: 0, y: yPoints)
+        }) { _ in
+            self.dismiss(animated: false)
+        }
+    }
+
+}
+
+extension UIView {
+
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
     }
 
 }
