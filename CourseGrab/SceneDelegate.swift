4be13ed90b2e980285bc6c1e6f7c52a85a3e7f2c
= <daniel.vebman@gmail.com>
2020-02-20 15:02:38 -0500

    Added firebase sign in

--- CourseGrab/SceneDelegate.swift
+++ CourseGrab/SceneDelegate.swift
@@ -6,53 +6,55 @@
 //  Copyright © 2020 Cornell AppDev. All rights reserved.
 //
 
+import FirebaseAuth
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = scene as? UIWindowScene else { return }
 
         let window = UIWindow(windowScene: scene)
-        window.rootViewController = MainNavigationController(rootViewController: LoginViewController())
+        let splash = UIViewController()
+        splash.view.backgroundColor = .white
+        window.rootViewController = splash
+
+        Auth.auth().addStateDidChangeListener { (auth, user) in
+            // TODO: Register user for notifications
+
+            let newVC = user == nil ? LoginViewController() : MainNavigationController(rootViewController: HomeTableViewController())
+            if let currentVC = self.topViewController() {
+                newVC.modalPresentationStyle = .fullScreen
+                currentVC.present(newVC, animated: true)
+            } else {
+                window.rootViewController = newVC
+            }
+        }
+
         self.window = window
         window.makeKeyAndVisible()
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
+    // MARK: - Helper
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
+    private func topViewController(viewController: UIViewController? = nil) -> UIViewController? {
+        let vc = viewController ?? window?.rootViewController
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
+        if let navigationController = vc as? UINavigationController {
+            if let topController = navigationController.topViewController {
+                return topViewController(viewController: topController)
+            }
+        } else if let tabController = vc as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(viewController: selected)
+            }
+        } else if let presented = vc?.presentedViewController {
+            return topViewController(viewController: presented)
+        }
 
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
+        return vc
     }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
 
 }
-
