a621fd8092edc4998fa32f0fa8267c97f652a2e7
mathewscullin <mjs698@cornell.edu>
2020-02-19 19:28:14 -0500

    Add result section header

---
+++ CourseGrab/ViewControllers/Search/SearchTableViewHeader.swift
@@ -0,0 +1,39 @@
+//
+//  SearchTableViewHeader.swift
+//  CourseGrab
+//
+//  Created by Mathew Scullin on 2/19/20.
+//  Copyright © 2020 Cornell AppDev. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewHeader: UITableViewHeaderFooterView {
+
+    private let titleLabel = UILabel()
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+
+        contentView.backgroundColor = .white
+        
+        titleLabel.font = ._20Semibold
+        titleLabel.textColor = .black
+        contentView.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().inset(6)
+            make.top.equalToSuperview().inset(24)
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(numResults: Int) {
+        titleLabel.text = "\(numResults) Results"
+    }
+    
+}
