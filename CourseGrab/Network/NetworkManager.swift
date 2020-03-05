cbe4ebc3230a077a11fb31f7e2ae206b3f830ca9
Kevin Chan <kevinchan159@gmail.com>
2020-03-04 20:32:57 -0500

    Adds fetching for device token

--- CourseGrab/Network/NetworkManager.swift
+++ CourseGrab/Network/NetworkManager.swift
@@ -5,40 +5,39 @@
 //  Created by Mathew Scullin on 2/25/20.
 //  Copyright © 2020 Cornell AppDev. All rights reserved.
 //
-
 import Foundation
 import FutureNova
 
 class NetworkManager {
-    
+
     private let networking: Networking = URLSession.shared.request
-    
+
     static let shared: NetworkManager = NetworkManager()
-    
+
     private init() { }
-    
+
     func intializeSession(googleToken: String) -> Future<Response<SessionAuthorization>> {
         return networking(Endpoint.initializeSession(with: googleToken)).decode()
     }
-    
+
     func updateSession() -> Future<Response<SessionAuthorization>> {
         return networking(Endpoint.updateSession()).decode()
     }
-    
+
     func getAllTrackedCourses() -> Future<Response<[Section]>> {
         return networking(Endpoint.getAllTrackedCourses()).decode()
     }
-    
+
     func trackCourse(catalogNum: Int) -> Future<Response<Section>> {
         return networking(Endpoint.trackCourse(catalogNum: catalogNum)).decode()
     }
-    
+
     func untrackCourse(catalogNum: Int) -> Future<Response<Section>> {
         return networking(Endpoint.untrackCourse(catalogNum: catalogNum)).decode()
     }
-    
+
     func searchCourse(query: String) -> Future<Response<[Course]>> {
         return networking(Endpoint.searchCourse(query: query)).decode()
     }
-    
+
 }
