0515707658ea063e4622ace8dc20e23fa3de7147
= <daniel.vebman@gmail.com>
2020-03-06 16:18:54 -0500

    Continue repairing constraint issues

--- CourseGrab/ViewControllers/Search/SearchTableViewController.swift
+++ CourseGrab/ViewControllers/Search/SearchTableViewController.swift
@@ -50,20 +50,25 @@ class SearchTableViewController: UITableViewController {
         ]
 
         courses = [
-            Course(courseNum: 124, subjectCode: "FWS", sections: [
-                Section(catalogNum: 0, courseNum: 0, isTracking: true, section: "", status: .closed, subjectCode: "FWS", title: ""),
-                Section(catalogNum: 0, courseNum: 0, isTracking: false, section: "", status: .closed, subjectCode: "FWS", title: ""),
-                Section(catalogNum: 0, courseNum: 0, isTracking: true, section: "", status: .closed, subjectCode: "FWS", title: "")
-            ], title: "Banana Republic: A Case Study in \n\n\n\nRaising Chickens in the Wild")
-
 //            Course(courseNum: 124, subjectCode: "FWS", sections: [
-//                Section(catalogNum: 0, courseNum: 0, isTracking: false, section: "", status: .closed, subjectCode: "FWS", title: "")
-//            ], title: "Banana Republic: A Case Study"),
+//                Section(catalogNum: 0, courseNum: 0, isTracking: true, section: "LEC 001 / MWF 10:10", status: .closed, subjectCode: "FWS", title: ""),
+//                Section(catalogNum: 0, courseNum: 0, isTracking: false, section: "LEC 001 / MWF 10:10", status: .closed, subjectCode: "FWS", title: ""),
+//                Section(catalogNum: 0, courseNum: 0, isTracking: true, section: "LEC 001 / MWF 10:10", status: .closed, subjectCode: "FWS", title: "")
+//            ], title: "Banana Republic: A Case Study in \n\n\n\nRaising Chickens in the Wild"),
 //
-//            Course(courseNum: 124, subjectCode: "FWS", sections: [
-//                Section(catalogNum: 0, courseNum: 0, isTracking: true, section: "", status: .closed, subjectCode: "FWS", title: "")
-//            ], title: "Banana Republic: A Case Study")
+            Course(courseNum: 124, subjectCode: "FWS", sections: [
+                Section(catalogNum: 0, courseNum: 0, isTracking: false, section: "LEC 001 / MWF 10:10", status: .closed, subjectCode: "FWS", title: "")
+            ], title: "Banana Republic: A Case Study"),
+
+            Course(courseNum: 124, subjectCode: "FWS", sections: [
+                Section(catalogNum: 0, courseNum: 0, isTracking: true, section: "LEC 001 / MWF 10:10", status: .closed, subjectCode: "FWS", title: ""),
+                Section(catalogNum: 0, courseNum: 0, isTracking: true, section: "LEC 001 / MWF 10:10", status: .closed, subjectCode: "FWS", title: "")
+            ], title: "Banana Republic: A Case Study")
         ]
+
+        for _ in 0..<4 {
+            courses += courses
+        }
     }
 
 }
