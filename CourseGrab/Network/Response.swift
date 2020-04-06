b8dd32d158e7ffba99be743624e1caeb3a3b1c18
Mathew Scullin <mjs698@cornell.edu>
2020-04-06 19:11:41 -0400

    Improve search (#55)
    * Improve search
    
    * Change timestamp type
    
    * Address PR comments
    
    * Add whitespace

--- CourseGrab/Network/Response.swift
+++ CourseGrab/Network/Response.swift
@@ -10,7 +10,20 @@ import Foundation
 
 struct Response<T: Codable>: Codable {
 
-    var data: T
-    var success: Bool
+    let data: T
+    let success: Bool
+    let timestamp: Date
+    
+}
 
+extension Response {
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        let rawDate = try values.decode(TimeInterval.self, forKey: .timestamp)
+        timestamp = Date(timeIntervalSince1970: rawDate)
+        success = try values.decode(Bool.self, forKey: .success)
+        data = try values.decode(T.self, forKey: .data)
+    }
+    
 }
